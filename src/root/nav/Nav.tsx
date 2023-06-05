6ecd45b3eb9480ac490b670190d1b7081090c98d
vlasa <vlasq91@gmail.com>
2023-06-05 10:13:22 +0300

    Added circle indicator of the active link

--- src/root/nav/Nav.tsx
+++ src/root/nav/Nav.tsx
@@ -1,26 +1,52 @@
 import { Link } from 'react-router-dom';
 import './Nav.scss';
 import { HashLink } from 'react-router-hash-link';
+import { MouseEvent, useState } from 'react';
 
 export default function Nav() {
+  const [indicatorStyle, setIndicatorStyle] = useState({});
+
+  const onLinkClick = (e: MouseEvent<HTMLAnchorElement>) => {
+    const target = e.target as HTMLElement;
+    const { left, width } = target.getBoundingClientRect();
+    console.log(left);
+    const newIndicatorStyle = {
+      left: `${left}px`,
+      width: `${width}px`
+    };
+    setIndicatorStyle(newIndicatorStyle);
+  };
+
   return (
     <nav>
+      <Link to={'/'} className="logo">
+        Vlasa Andrei
+      </Link>
       <ul className="links">
         <li>
-          <HashLink smooth to={'/#about'}>
+          <HashLink onClick={onLinkClick} smooth to={'/#about'}>
             About
           </HashLink>
         </li>
         <li>
-          <HashLink to={'/#projects'}> Projects </HashLink>
+          <HashLink onClick={onLinkClick} to={'/#projects'}>
+            Projects
+          </HashLink>
         </li>
         <li>
-          <HashLink to={'/#contact'}> Contact </HashLink>
+          <HashLink onClick={onLinkClick} to={'/#contact'}>
+            Contact
+          </HashLink>
         </li>
         <li>
-          <Link to={'/blog'}> Blog </Link>
+          <Link onClick={onLinkClick} to={'/blog'}>
+            Blog
+          </Link>
         </li>
       </ul>
+      <div className="indicator" style={indicatorStyle}>
+        <div className="circle"></div>
+      </div>
     </nav>
   );
 }
